3c2010e1e4b320ce584f7a9b1a1cd082e1e1d7ed
prototypedave <popupgfj@gmail.com>
2022-09-20 18:57:24 -0700

    node basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+//prints a given string in STDOUT
+
+const displayMessage = function displayMessage(message){
+    console.log(message);
+}
+
+// export variable message
+module.exports = displayMessage;
