08e7dd7e91f0bf33e9ce4ded0e3d4a30474417f7
prototypedave <popupgfj@gmail.com>
2022-09-23 19:40:17 -0700

    errors

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const router = express.Router();
-
 import AppController from '../controllers/AppController';
 import StudentsController from '../controllers/StudentsController';
+
+const express = require('express');
+const router = express.Router();
 
 router.get('/', (req, res) => AppController.getHomepage(req, res));
 router.get('/students', (req, res) => StudentsController.getAllStudents(req, res));
